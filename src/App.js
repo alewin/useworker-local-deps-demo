a38d780d124b4956e6c7b954b38b3c4d4578cc8b
Alessio Koci <ko.alessio@gmail.com>
2021-01-23 12:03:31 +0100

    feat: capitalize example

--- src/App.js
+++ src/App.js
@@ -1,67 +1,30 @@
-import React from "react";
-import { useWorker, WORKER_STATUS } from "@koale/useworker";
+import React, { useState } from "react";
 
-import bubleSort from "./bublesort";
+import { useWorker } from "@koale/useworker";
 
-const demoFunction = (numbers) => {
-  const result = bubleSort(numbers)
-  return result
-}
+import { capitalize } from "./capitalize";
+
 
 function LocalDeps() {
-
-  const [workerFn, {
-    status: workerFnStatus,
-    kill: killWorker
-  }] = useWorker(demoFunction, { 
-    localDependencies: () => [bubleSort],
-    autoTerminate: false
+  const [text, setText] = useState("");
+  const [result, setResult] = useState("");
+  const capFn = (text) => capitalize(text);
+  const [wCapitalize] = useWorker(capFn, {
+    localDependencies: () => [capitalize]
   });
 
-  React.useEffect(()=>{
-    console.log("WORKER:", workerFnStatus);
-  }, [workerFnStatus])
-
-  const onWorkerClick = () => {
-    const numbers = [...Array(50000)].map(() =>
-  Math.floor(Math.random() * 1000000)
-);
-    workerFn(numbers).then(result => {
-      console.log("localdeps useWorker()", result);
-      alert("Finished: localdeps using useWorker.", { appearance: "success" });
-    });
-  };
-
   return (
-    <div>
-      <section className="App-section">
-
-        <button
-          type="button"
-          disabled={workerFnStatus === WORKER_STATUS.RUNNING}
-          className="App-button"
-          onClick={() => onWorkerClick()}
-        >
-          {workerFnStatus === WORKER_STATUS.RUNNING
-            ? `Loading...`
-            : `Transferable useWorker()`}
-        </button>
-        {workerFnStatus === WORKER_STATUS.RUNNING ? (
-          <button
-            type="button"
-            className="App-button"
-            onClick={() => killWorker()}
-          >
-            Kill Worker
-          </button>
-        ) : null}
-      </section>
-      <section className="App-section">
-        <span style={{ color: "white" }}>
-          Open DevTools console to see the results.
-        </span>
-      </section>
-    </div>
+    <>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button
+        onClick={(_) => {
+          wCapitalize(text).then(setResult);
+        }}
+      >
+        Go
+      </button>
+      {result}
+  </>
   );
 }
 
---
+++ src/capitalize.js
@@ -0,0 +1,1 @@
+export const capitalize = (text = "") => text.toUpperCase();

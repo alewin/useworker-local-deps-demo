d28e447e35284bd96b4a23f8a4aab443b6a7deac
Alessio Koci <ko.alessio@gmail.com>
2021-01-23 11:55:58 +0100

    feat: useworker demo local deps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useWorker, WORKER_STATUS } from "@koale/useworker";
 
-function App() {
+import bubleSort from "./bublesort";
+
+const demoFunction = (numbers) => {
+  const result = bubleSort(numbers)
+  return result
+}
+
+function LocalDeps() {
+
+  const [workerFn, {
+    status: workerFnStatus,
+    kill: killWorker
+  }] = useWorker(demoFunction, { 
+    localDependencies: () => [bubleSort],
+    autoTerminate: false
+  });
+
+  React.useEffect(()=>{
+    console.log("WORKER:", workerFnStatus);
+  }, [workerFnStatus])
+
+  const onWorkerClick = () => {
+    const numbers = [...Array(50000)].map(() =>
+  Math.floor(Math.random() * 1000000)
+);
+    workerFn(numbers).then(result => {
+      console.log("localdeps useWorker()", result);
+      alert("Finished: localdeps using useWorker.", { appearance: "success" });
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <section className="App-section">
+
+        <button
+          type="button"
+          disabled={workerFnStatus === WORKER_STATUS.RUNNING}
+          className="App-button"
+          onClick={() => onWorkerClick()}
         >
-          Learn React
-        </a>
-      </header>
+          {workerFnStatus === WORKER_STATUS.RUNNING
+            ? `Loading...`
+            : `Transferable useWorker()`}
+        </button>
+        {workerFnStatus === WORKER_STATUS.RUNNING ? (
+          <button
+            type="button"
+            className="App-button"
+            onClick={() => killWorker()}
+          >
+            Kill Worker
+          </button>
+        ) : null}
+      </section>
+      <section className="App-section">
+        <span style={{ color: "white" }}>
+          Open DevTools console to see the results.
+        </span>
+      </section>
     </div>
   );
 }
 
-export default App;
+export default LocalDeps;
